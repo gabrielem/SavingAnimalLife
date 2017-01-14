53b0b38bd8b22afc6fc54513f7750310dc33fcd7
gabrielem_ <gabriele.marazzi@gmail.com>
2017-01-14 22:28:27 +0100

    Addint text

--- js/_main.js
+++ js/_main.js
@@ -18,4 +18,39 @@ $(function() {
         text : i
     }));
   }
+
+
+  var setDonation = function(){
+    console.log("setDonation");
+    var fishPrice = 1.50;
+    var goatPrice = 145;
+
+      var fishs = $('.js-fishs-select').val()
+      var goats = $('.js-goats-select').val()
+      if(fishs>0 || goats>0){
+        /*
+          Set a donation items
+        */
+        $(".js-noDonationInfo").hide()
+        $(".js-sendDonation").show()
+        var fishsPrice = fishPrice*fishs
+        var goatsPrice = goatPrice*goats
+
+        donationPrice = fishsPrice+goatsPrice
+        $("#donationPrice").val("£" + donationPrice)
+
+      }else{
+        /*
+          no donation item found
+        */
+        $(".js-noDonationInfo").show()
+        $(".js-sendDonation").hide()
+      }
+  }
+  $('.js-fishs-select').on('change', function() {
+    setDonation()
+  })
+  $('.js-goats-select').on('change', function() {
+    setDonation()
+  })
 });
